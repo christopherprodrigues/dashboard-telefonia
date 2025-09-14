1227413fd5925d7aaf45641490d7372bc41dbcb8
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-09-13 21:30:08 -0300

    Fix import path in models.py and add Alembic configuration files for database migrations

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from .database import Base
+from app.dabatase import Base
 
 class User(Base):
     __tablename__ = "users"
