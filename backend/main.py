30972e44d675e99fbb4b0e652ec98aebe75cac8d
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-09-14 14:18:31 -0300

    Add main application file and include authentication and user management routers

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+from app.main import app
+from routers import auth, users
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import FastAPI
 
 app = FastAPI(
@@ -5,6 +12,9 @@ app = FastAPI(
     description="API para gerenciar usuários e visualizar métricas de chamadas.",
     version="1.0.0"
 )
+
+app.include_router(auth.router)
+app.include_router(users.router)
 
 @app.get("/", tags=["Root"])
 async def read_root():
