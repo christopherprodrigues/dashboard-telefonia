d1f3193fff4ff9280110c68daac6f385ca665cb9
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-09-14 12:40:56 -0300

    Implement security module with JWT authentication and password hashing functions

---
+++ backend/app/security.py
@@ -0,0 +1,36 @@
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+from dotenv import load_dotenv
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+load_dotenv()
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Verifica se a senha fornecida corresponde ao hash salvo."""
+    return pwd_context.verify(plain_password, hashed_password) 
+
+def get_password_hash(password: str) -> str:
+    """Gera o hash de uma senha em texto puro."""
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """Cria um novo token de acesso (JWT)."""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
